7ec2a145e9a410ff3568fed4b94844c37b79fdad
Ariel T. Glenn <ariel@wikimedia.org>
2026-07-28 19:48:40 -0700

    initial commit
    This is a toy service only, with no error checks and no security checks
    either. USE AT YOUR OWN RISK.

---
+++ README.md
@@ -0,0 +1,28 @@
+openapi-eightball
+=================
+
+This is a toy REST service that acts like a digital magic eight ball
+able to respond to one of a small fixed set of questions.
+
+It is not safe, secure, or guaranteed to be functional for anything but the
+specific queries it supports, and that is all. See the terms_of_use.html
+file in this repository for more.
+
+Start it by running, in a terminal window:
+  python3 eightball.py
+You will then be able to make queries to 127.0.1:8080 via a browser or curl.
+Go to the url http://127.0.0.1:8080/8ball/v0/help for available queries.
+
+If you want to run a second copy of it on another port, or a different host, you
+may do so by running
+  python3 eightball.py --host server-name-here --port port-number-here
+
+The toy service has an accompanying OpenAPI spec file.
+
+The terms of use and spec files are intended to be hosted on a web server
+on your desktop or laptop locally.
+
+This service is an example for use wth the MediaWiki Rest Sandbox,
+see https://www.mediawiki.org/wiki/Help:RestSandbox for the Sandbox,
+and TBD for using this demo service and the included OpenAPI spec with
+the Sandbox on a local installation of MediaWiki.
---
+++ eightball.py
@@ -0,0 +1,126 @@
+#!/usr/bin/python3
+'''
+magic 8 ball questions and answers module
+this runs on port 80 and answers REST requests
+for a predefined set of questions
+
+this is a toy for local testing of OpenAPI stuffs only,
+and if you ever expose it to other users, that
+is Very Bad. Do Not Do That.
+'''
+import argparse
+import json
+import random
+from urllib import parse
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+class EightBallRequestHandler(BaseHTTPRequestHandler):
+    '''
+    process an incoming HTTP request for a query to the magic 8 ball.
+    '''
+    API_BASE = '/8ball/v0/'
+    TOPICS = {'romance': 'Will I have a great date within the next 2 weeks?',
+              'puppy': 'Will a wonderful puppy enter my life within the next 4 weeks?',
+              'kitty': 'Will a wonderful kitty enter my life within the next 4 weeks?',
+              'Bug1': 'Will Bug 1 be completely fixed within the next 6 months?',
+              'singularity': 'Will The Singularity(TM) happen within the next 6 months?',
+              'movie': 'Will the next movie I see be worth the price?',
+              'wwu': 'Will the WMF voluntarily recognize the WWU US branch?'}
+    ANSWERS = ['It is certain', 'It is decidedly so', 'Without a doubt',
+               'Yes definitely', 'You may rely on it', 'As I see it, yes',
+               'Most likely', 'Outlook good', 'Yes', 'Signs point to yes',
+               'Reply hazy, try again', 'Ask again later', 'Better not tell you now',
+               'Cannot predict now', 'Concentrate and ask again', "Don't count on it",
+               'My reply is no', 'My sources say no', 'Outlook not so good',
+               'Very doubtful']
+    HELP = {'help': 'example: curl "http://127.0.0.1:8080/8ball/v0/help"',
+            'topics': 'example: curl "http://127.0.0.1:8080/8ball/v0/topics',
+            'question': 'example: curl "http://127.0.0.1:8080/8ball/v0/question/puppy'}
+
+    def get_ball_answer(self):
+        '''
+        get an 8 ball random answer, independently of whatever question is asked.
+        random seed is the default (time)
+        '''
+        return self.ANSWERS[random.randrange(0, len(self.ANSWERS)-1)]
+
+    def get_answer(self, topic):
+        '''
+        given a topic (question short form), return the full text of the question
+        and the 8 ball answer
+        '''
+        ball_answer = self.get_ball_answer()
+        return json.dumps({'question': self.TOPICS[topic],
+                           'answer': ball_answer})
+
+    def path_invalid(self, components, query):
+        '''
+        check prefix, number of components, and expect no query string
+        '''
+        if len(components) == 4:
+            return self.topic_invalid(components[3])
+        return len(components) != 3 or not self.path.startswith(self.API_BASE) or query
+
+    def topic_invalid(self, topic):
+        '''
+        check that topic is a known query or one of the defined topics
+        '''
+        return topic not in self.TOPICS and topic != 'topics' and topic != 'help'
+
+    def get_response(self):
+        '''
+        get the body of the response for q request for the list of questions
+        or the request for an answer to a specific questions
+        '''
+        url_fields = parse.urlsplit(self.path.lstrip('/'))
+        # url_fields[2] is the path up to the ? if any
+        path_components = url_fields[2].split('/')
+        if self.path_invalid(path_components, query=url_fields[3]):
+            self.send_error(404, message='Not Found',
+                            explain='No such content available: ' + path_components[2])
+            return ''
+        if path_components[2] == 'topics':
+            return json.dumps(self.TOPICS)
+        if path_components[2] == 'help':
+            return json.dumps(self.HELP)
+        if path_components[2] == 'question':
+            return self.get_answer(path_components[3])
+        self.send_error(404, message='Not Found',
+                        explain='No such content available: ' + path_components[2])
+        return ''
+
+
+    def do_GET(self):   # pylint: disable=invalid-name
+        '''
+        override the GET method handler for the parent class
+        '''
+        self.send_response(200)
+        self.send_header("Content-Type", "application/json")
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.end_headers()
+        self.wfile.write(self.get_response().encode("utf-8"))
+
+    def do_POST(self):  # pylint: disable=invalid-name
+        '''
+        override the POST method handler for the parent class
+        in our case, we just don't support POST requests at all
+        '''
+        self.send_error(405, message="POST method unsupported", explain="")
+
+
+def do_main():
+    '''
+    entry point
+    '''
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--host", type=str, default="0.0.0.0")
+    parser.add_argument("--port", type=int, default=8080)
+    args = parser.parse_args()
+
+    server = HTTPServer((args.host, args.port), EightBallRequestHandler)
+    server.serve_forever()
+
+
+if __name__ == "__main__":
+    do_main()
